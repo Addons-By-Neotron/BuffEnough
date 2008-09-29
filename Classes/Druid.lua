4411709716ae14825eb5f633a54a50637d358c76
root <root@e036d3ba-f761-4992-8c97-3dad5f5997e8>
2008-09-29 20:57:40 +0000

    Importing old repo data under /trunk

---
+++ Classes/Druid.lua
@@ -0,0 +1,56 @@
+--[[ ---------------------------------------------------------------------------
+
+BuffEnough: personal buff monitor.
+
+BuffEnough is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+BuffEnough is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BuffEnough.  If not, see <http://www.gnu.org/licenses/>.
+
+----------------------------------------------------------------------------- ]]
+
+
+if select(2, UnitClass("player")) ~= "DRUID" then return end 
+
+local L = LibStub("AceLocale-3.0"):GetLocale("BuffEnough")
+local Druid = BuffEnough:GetOrCreateModule("Player")
+
+
+--[[ ---------------------------------------------------------------------------
+     Check class buffs
+----------------------------------------------------------------------------- ]]
+function Druid:CheckClassBuffs()
+
+    if BuffEnough.debug then BuffEnough:debug("Checking druid buffs") end
+
+    local omenOfClarity = select(5, GetTalentInfo(3, 8, false))
+
+    if omenOfClarity > 0 then
+        BuffEnough:TrackItem(L["Buffs"], BuffEnough.spells["Omen of Clarity"], false, true, false, nil, nil, true)
+    end
+
+end
+
+--[[ ---------------------------------------------------------------------------
+     Formulate priority list for paladin blessings
+----------------------------------------------------------------------------- ]]
+function Druid:GetPaladinBlessingList()
+
+    if select(3, GetTalentTabInfo(1)) > 30 then
+        return {BuffEnough.spells["Blessing of Salvation"], BuffEnough.spells["Blessing of Wisdom"], BuffEnough.spells["Blessing of Kings"], BuffEnough.spells["Blessing of Sanctuary"], BuffEnough.spells["Blessing of Light"], BuffEnough.spells["Blessing of Might"]}
+    elseif select(3, GetTalentTabInfo(2)) > 30 then
+        return {BuffEnough.spells["Blessing of Salvation"], BuffEnough.spells["Blessing of Kings"], BuffEnough.spells["Blessing of Might"], BuffEnough.spells["Blessing of Sanctuary"], BuffEnough.spells["Blessing of Light"], BuffEnough.spells["Blessing of Wisdom"]}
+    else
+        return {BuffEnough.spells["Blessing of Salvation"], BuffEnough.spells["Blessing of Wisdom"], BuffEnough.spells["Blessing of Kings"], BuffEnough.spells["Blessing of Sanctuary"], BuffEnough.spells["Blessing of Light"], BuffEnough.spells["Blessing of Might"]}
+    end
+
+end
+
