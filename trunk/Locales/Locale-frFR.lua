fe35f1015aceb0642f66be9bbad0e4beb9008f25
dhedbor <dhedbor@e036d3ba-f761-4992-8c97-3dad5f5997e8>
2010-11-27 23:52:22 +0000

    Fixed localization path.

--- trunk/Locales/Locale-frFR.lua
+++ trunk/Locales/Locale-frFR.lua
@@ -3,7 +3,7 @@ if not L then return end
 
 -- To help localize BuffEnough please enter phrase translations on the
 -- following URL:
--- http://www.wowace.com/projects/magicrunes/localization/
+-- http://www.wowace.com/projects/buff-enough/localization/
 -- This file should not be edited manually!
 
 --@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="handle-subnamespaces")@
